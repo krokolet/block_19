a05a209497a8648bcabda518f8ba13584c8c7e3f
krokolet <46382039+krokolet@users.noreply.github.com>
2020-07-13 12:05:53 +0700

    Add files via upload

---
+++ precache-manifest.2c77f87328f1da9b4b4d8b9ed3bb8a03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a60a81ffa82ce2620be9d2d9d51c7a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e13ce868b78ebe682b0a",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "53395b2e9146b9b8789a",
+    "url": "/static/css/main.325bc2dc.chunk.css"
+  },
+  {
+    "revision": "e13ce868b78ebe682b0a",
+    "url": "/static/js/2.eb6291b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.eb6291b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53395b2e9146b9b8789a",
+    "url": "/static/js/main.dbf39cf0.chunk.js"
+  },
+  {
+    "revision": "cb8e000bf916c88e000c",
+    "url": "/static/js/runtime-main.b2fa1274.js"
+  },
+  {
+    "revision": "c5fcf8456b31edcf9d2d565266b78b2d",
+    "url": "/static/media/logo.c5fcf845.png"
+  }
+]);
